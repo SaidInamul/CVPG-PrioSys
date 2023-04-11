6ab0db782735e01c9052009004022a0ce47ae5f9
SaidInamul <100667330+SaidInamul@users.noreply.github.com>
2023-04-12 02:25:50 +0800

    registration.html
    implement registration page (structure)

---
+++ library/js/secondary.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	console.log("Hello world");
+
+	// const btn = document.querySelector("#btnSignUp");
+
+	// btn.classList.add("spinner");
+	// btn.classList.remove("button--loading");
+});
